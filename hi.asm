9db4ff02fdfebc149a4b581e5bd3334430819660
JM <jmjanzen@gmail.com>
2016-10-20 12:55:37 -0400

    Add many explanatory comments
    While here, assign msg len a bit more explicitly

--- hi.asm
+++ hi.asm
@@ -1,17 +1,19 @@
 ; export start for linker (nasm specific)
-global _start
+global _start  ; indicates where program execution begins
 
 ;
 ;   General 32 bit  registers:
 ;       EAX, EBX, ECX, EDX
 ;   Commands:
 ;       mov - assign second part to first part.
-;       int - software interrupt (0x80 = system call).
+;       int - software interrupt (0x80 = system call),
+;             determined by value in EAX register.
 ;
 
+; declare source code in section.text
 section .text
 _start:
-    mov eax, 4 ; write
+    mov eax, 4 ; 4 = write, 3 = read
     mov ebx, 1 ; stdout
     mov ecx, msg
     mov edx, msg.len
@@ -21,7 +23,16 @@ _start:
     mov ebx, 0
     int 0x80   ; exit(0)
 
+; declare constants here in section.data
 section .data
+; breakdown of below:
+;   label -> msg
+;   db -> psuedo-instruction which accepts strs, chars, and nums
+;   10 -> newline
+;   msg.len -> declares len = (curr_location - msg_location)
 msg: db "Hello World!", 10
-.len: equ $ - msg
+msg.len: equ $ - msg
+
+; declare variables in section.bss
+section .bss
 
